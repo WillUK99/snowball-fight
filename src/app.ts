49a10e56f595561d82f33587048a95a4bf59b72f
WillUK99 <76099444+WillUK99@users.noreply.github.com>
2022-11-30 21:38:20 +0000

    chore: make 2d map array

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,44 @@
 import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+const tmx = require('tmx-parser')
 
 const app = express()
 const httpServer = createServer(app)
 const io = new Server(httpServer)
 
-io.on('connection', (socket) => {
-  console.log(socket.id)
-})
+async function main() {
+  const map: any = await new Promise((resolve, reject) => {
+    tmx.parseFile('./src/map.tmx', (err: Error, loadedMap: any) => {
+      if (err) return reject(err)
+      resolve(loadedMap)
+    })
+  })
 
-app.use(express.static('public'))
+  const layer = map.layers[0]
+  const tiles = layer.tiles
+  // console.log(tiles)
+  const map2D: any[] = []
 
-httpServer.listen(1337)
+  for (let row = 0; row < map.width; row++) {
+    const tileRow: any[] = []
+    for (let column = 0; column < map.height; column++) {
+      const tile = tiles[row * map.height + column]
+      map2D.push({ id: tile.id, gid: tile.gid })
+    }
+    map2D.push(tileRow)
+  }
+
+  console.log('***', map2D)
+
+  io.on('connect', (socket) => {
+    io.emit('mapData', map2D)
+  })
+
+  app.use(express.static('public'))
+
+  httpServer.listen(1337)
+}
+
+main()
+
