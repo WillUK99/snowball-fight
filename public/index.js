933ac85aa7d91e8250b569bfaed3c48a0bb9082c
WillUK99 <76099444+WillUK99@users.noreply.github.com>
2022-12-05 08:42:28 +0000

    feat: draw map in game loop

--- public/index.js
+++ public/index.js
@@ -1,9 +1,51 @@
+const mapImage = new Image()
+mapImage.src = '/snowy-sheet.png'
+
+const canvasEl = document.getElementById('canvas')
+const ctx = canvasEl.getContext('2d')
+canvasEl.width = window.innerWidth
+canvasEl.height = window.innerHeight
+
+let map = [[]]
+const TILE_SIZE = 16
+const TILES_IN_ROW = 8
+
+window.addEventListener('resize', () => {
+  canvasEl.width = window.innerWidth
+  canvasEl.height = window.innerHeight
+})
+
 const socket = io(`ws://localhost:1337`)
 
 socket.on('connect', () => {
   console.log('connected')
 })
 
-socket.on('2D Map', (mapData) => {
-  console.log('***', mapData)
-})
+socket.on('2D Map', (mapData) => map = mapData)
+
+const gameLoop = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (let row = 0; row < map.length; row++) {
+    for (let column = 0; column < map[0].length; column++) {
+      const { id } = map[row][column]
+      const imageRow = parseInt(id / TILES_IN_ROW, 10)
+      const imageColumn = parseInt(id % TILES_IN_ROW, 10)
+      ctx.drawImage(
+        mapImage,
+        imageColumn * TILE_SIZE,
+        imageRow * TILE_SIZE,
+        TILE_SIZE,
+        TILE_SIZE,
+        column * TILE_SIZE,
+        row * TILE_SIZE,
+        TILE_SIZE,
+        TILE_SIZE,
+      )
+    }
+  }
+
+  requestAnimationFrame(gameLoop)
+}
+
+requestAnimationFrame(gameLoop)
